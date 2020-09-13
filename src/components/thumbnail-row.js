3f4f71942756ac233066f4875049c15d1b80e9ee
Wes Johnson <wjohnson@wealthsimple.com>
2020-09-13 18:23:43 -0400

    rejig thumbnailRow for responsiveness

--- src/components/thumbnail-row.js
+++ src/components/thumbnail-row.js
@@ -1,61 +1,70 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'gatsby'
 
 const maxImageWidth = 180
 const maxImageHeight = 120
 
-const styleForPlaceholder = {
-  backgroundColor: '#bbb',
-  backgroundSize: 'cover',
-  opacity: 0.3,
-}
+const styleForPlaceholder = `
+  background-color: #bbb;
+  background-size: cover;
+  opacity: 0.3;
+`
 
-const styleForFeatureImage = {
-  backgroundSize: 'contain',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-}
+const styleForFeatureImage = `
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`
 
-const Thumbnail = ({ uri, style }) => (
-  <div style={style}>
-    {
-      <div
-        style={{
-          width: maxImageWidth,
-          height: maxImageHeight,
+const Thumbnail = styled('div')`
+  width: ${maxImageWidth}px;
+  height: ${maxImageHeight}px;
+  width: 200px;
+  textalign: center;
 
-          backgroundImage: uri
-            ? `url('${uri}')`
-            : `url('${require('../assets/logo+transparent+grey.png')}')`,
+  background-image: ${props =>
+    props.uri
+      ? `url('${props.uri}')`
+      : `url('${require('../assets/logo+transparent+grey.png')}')`};
 
-          borderTopLeftRadius: 3,
-          borderBottomLeftRadius: 3,
-          ...(uri ? styleForFeatureImage : styleForPlaceholder),
-        }}
-      />
-    }
-  </div>
-)
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+  ${props => (props.uri ? styleForFeatureImage : styleForPlaceholder)}
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+  min-height: ${maxImageHeight}px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const RightSide = styled('div')`
+  margin-left: 20px;
+  padding-top: 15px;
+`
 
 export const ThumbnailRow = ({ imageUri, title, description, link }) => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'row',
-      marginBottom: 20,
-      minHeight: maxImageHeight,
-    }}
-  >
+  <Container>
     <Link to={link}>
-      <Thumbnail uri={imageUri} style={{ width: 200, textAlign: 'center' }} />
+      <Thumbnail uri={imageUri} />
     </Link>
-    <div style={{ marginLeft: 20, paddingTop: 15 }}>
+    <RightSide>
       <h3>
         <Link to={link} style={{ color: '#3275b8', textDecoration: 'none' }}>
           {title}
         </Link>
       </h3>
       <p>{description}</p>
-    </div>
-  </div>
+    </RightSide>
+  </Container>
 )

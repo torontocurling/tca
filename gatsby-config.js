072374cb49bb24bd345da6455f0994ed20a4c8c6
Wes Johnson <wjohnson@wealthsimple.com>
2020-09-13 18:52:02 -0400

    add GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-45726725-2`,
       },
     },
     // `gatsby-plugin-feed`,
